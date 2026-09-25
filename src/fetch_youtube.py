89bb649e3399ff33032fe267ae98b9e6ac156694
Derek Dao <DerekDao01@gmail.com>
2026-09-24 20:47:31 -0700

    write get_channel_details

---
+++ src/fetch_youtube.py
@@ -0,0 +1,44 @@
+"""Fetches creator data from YouTube using YouTube's Data API V3.
+"""
+import os
+
+from dotenv import load_dotenv
+import googleapiclient.discovery
+import googleapiclient.errors
+
+load_dotenv()
+
+api_key = os.getenv("YOUTUBE_API_KEY")
+youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
+
+def get_channel_details(youtube, handle):
+    """Takes a channel handle (e.g., "@mkbhd").
+        Calls channels().list(forHandle=handle, part="snippet,contentDetails").
+        Returns:
+        channel_id (e.g. UC...)
+        channel_title (e.g. Marques Brownlee)
+        uploads_playlist_id (e.g. UU...)"""
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        forHandle=handle
+    )
+    response = request.execute()
+
+    print(response)
+
+
+def get_recent_video_ids(youtube, uploads_playlist_id, max_results=10):
+    """Calls playlistItems().list(playlistId=uploads_playlist_id, 
+                                    maxResults=max_results, 
+                                    part="contentDetails").
+        Extracts and returns a list of video ID strings: ['dQw4w9WgXcQ', ...]."""
+    pass
+
+def get_video_snapshots(youtube, video_ids, channel_info):
+    """Calls videos().list(id=",".join(video_ids), part="snippet,statistics").
+        Returns clean, structured dictionary objects representing each video snapshot."""
+    pass
+
+
+get_channel_details(youtube, '@mkbhd')
